f7eea37caee6d277a09f5609393a00fe306ddda3
Prashant Chakurkar <55869805+Parshchakurkar@users.noreply.github.com>
2019-11-17 11:25:30 +0000

    Update prashant.c

--- prashant.c
+++ prashant.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+Int main ()
+{
+  Int n,r=0,rev=0;
+  print("enter no");
+  scanf("%d",&n);
+  while(n>0)
+  {
+    r=n℅10;
+    n=n/10;
+    rev=rev*10+r;
+  }
+  printf("%d",rev);
+  return 0;
+}
